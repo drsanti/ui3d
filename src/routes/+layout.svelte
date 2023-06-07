b56f9c4c46f27ad76a881a8ee5d27f2e474b2d88
drsanti <santi.inc.kmutt@gmail.com>
2023-06-07 09:55:12 +0700

    add docs as a static site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,12 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	//
+	import { updated } from '$app/stores';
 </script>
 
-<slot />
+<!-- <slot /> -->
+<main data-sveltekit-reload={$updated ? '' : 'off'}>
+	<slot />
+</main>
