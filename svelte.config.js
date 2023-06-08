19c35764c7105a0808a278b88b42af7575478161
drsanti <santi.inc.kmutt@gmail.com>
2023-06-08 13:27:59 +0700

    add docs directory

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@
 // 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 // 	// for more information about preprocessors
 // 	preprocess: vitePreprocess(),
-	
+
 // 	vitePlugin: {
 // 		inspector: true,   
 // 	},
@@ -26,23 +26,23 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	
+
 	vitePlugin: {
-		inspector: true,   
+		inspector: true,
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'docs',//'ui32-pages',//'build',
+			assets: 'docs',//'ui32-pages',//'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 export default config;
